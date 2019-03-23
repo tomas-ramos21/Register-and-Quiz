f37d529c9dbc5bcb0ffcddf9da8526c76d30813f
Tomas Ramos <tomas.ramosv21@gmail.com>
2019-03-23 18:03:03 +0800

    Moved Bulding and Room models to admin app. Updated teaching spaces html

--- administrative/models.py
+++ administrative/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+
+class Building(models.Model):
+
+	"""
+		Django's Model to represent the building
+		class in the MySQL database.
+	"""
+
+	code	= models.CharField(primary_key=True, max_length=4)	# Building's Code
+	name	= models.CharField(max_length=20)			# Building's Name
+
+class Room(models.Model):
+
+        """
+                Django's Model to represent the room
+                class in MySQL database.
+        """
+
+        id      = models.CharField(primary_key=True, max_length=10)     # Room's ID
+        code    = models.PositiveIntegerField()                         # Room's Code
+        bd_code = models.ForeignKey(Building, on_delete=models.PROTECT)  # Building Code
+        level   = models.IntegerField()                                 # Room's level/Floor
+        capacity= models.PositiveIntegerField()                         # Room's Capacity
--- login/models.py
+++ login/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from administrative.models import Building, Room
 
 class Course(models.Model):
 
@@ -22,29 +23,6 @@ class Unit(models.Model):
 	title 	= models.CharField(max_length=30)			# Unit Title
 	credits = models.PositiveIntegerField()				# Unit's max credits
 	image = models.CharField(max_length=50, default='default.jpg')
-
-class Building(models.Model):
-
-	"""
-		Django's Model to represent the building
-		class in the MySQL database.
-	"""
-
-	code	= models.CharField(primary_key=True, max_length=4)	# Building's Code
-	name	= models.CharField(max_length=20)			# Building's Name
-
-class Room(models.Model):
-
-        """
-                Django's Model to represent the room
-                class in MySQL database.
-        """
-
-        id      = models.CharField(primary_key=True, max_length=10)     # Room's ID
-        code    = models.PositiveIntegerField()                         # Room's Code
-        bd_code = models.ForeignKey(Building, on_delete=models.PROTECT)  # Building Code
-        level   = models.IntegerField()                                 # Room's level/Floor
-        capacity= models.PositiveIntegerField()                         # Room's Capacity
 
 class Teaching_Period(models.Model):
 
