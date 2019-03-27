c9bdc250f2568c2042df4ac02eb2e19371bee6b9
roumanite <madyagra@gmail.com>
2019-03-28 01:12:02 +0800

    Grace changes the administrative urls.py

--- administrative/urls.py
+++ administrative/urls.py
@@ -8,10 +8,10 @@ app_name = 'administrative'
 
 urlpatterns = [
 	path('my/', views.admin_home, name='admin_home'),
-	url(r'^accountM/$', views.acc_management, name='acc_management'),
-	url(r'^unitsM/$', views.unit_management, name='unit_management'),
-	url(r'^teachingspace/$', views.space_management, name='space_management'),
-	url(r'^lectAccAdd.html/$', views.employee_creation, name='employee_creation'),
-	url(r'^stuAccAdd.html/$', views.student_creation, name='student_creation'),
+	url(r'^account-management/$', views.acc_management, name='acc_management'),
+	url(r'^unit-management/$', views.unit_management, name='unit_management'),
+	url(r'^teaching-space-management/$', views.space_management, name='space_management'),
+	url(r'^create-lecturer/$', views.employee_creation, name='employee_creation'),
+	url(r'^create-student/$', views.student_creation, name='student_creation'),
 	url('user_login', views.user_logout, name='user_logout'),
 ]
