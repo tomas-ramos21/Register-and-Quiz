c9ebe4253368f229f2ae756b4450eb8301efd244
john <johnwee35@gmail.com>
2019-03-31 22:08:35 +0800

    change static page

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,10 +1,11 @@
-$(function(){ // iffee function
-  
-    $(".dropdown-menu a").click(function(){ // dropdown menu for selecting a function
-      
-      $(".dropdown-btn:first-child").text($(this).text()); // change the first button text to this text
-       $(".dropdown-btn:first-child").val($(this).text());
-    });
-  
+
+
+  $(".dropdown-first a").click(function(){
+    // var selText = $(this).text();
+    $(".stats-btn").text($(this).text());
   });
 
+  $(".dropdown-second a").click(function(){
+    // var selText = $(this).text();
+    $(".stats-btn2").text($(this).text());
+  });
