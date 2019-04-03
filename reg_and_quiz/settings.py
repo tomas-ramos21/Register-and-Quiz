c1b6831b84a9de69b44826524c299e11ee41866e
Ubuntu <ubuntu@ip-172-31-0-67.ap-southeast-1.compute.internal>
2019-04-03 03:07:26 +0000

    Updated IP.

--- reg_and_quiz/settings.py
+++ reg_and_quiz/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = '32ay1u!(pw)=094ijgcm_(!fwo2gn5uq^n_i5m6hka5gqg#jq7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-13-229-149-79.ap-southeast-1.compute.amazonaws.com']
-
+ALLOWED_HOSTS = ['ec2-13-229-154-134.ap-southeast-1.compute.amazonaws.com']
 
 # Application definition
 
